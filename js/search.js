6ce0dc172178af95be582b9dfe1e6bd5fff1fcb7
taehyeon0412 <71374539+taehyeon0412@users.noreply.github.com>
2022-07-08 05:25:00 +0900

    검색창 구현+css

---
+++ js/search.js
@@ -0,0 +1,15 @@
+/*const searchForm=document.querySelector(".search-form");
+const searchInput=searchForm.querySelector("input");
+
+
+
+function searchSubmit(){
+    if(window.location.reload()){
+        searchInput.value="";
+    }
+}
+
+searchForm.addEventListener("submit",searchSubmit);
+
+
+/*submit 후에 뒤로가기 눌렀을때 input값이 초기화되게 만들어야됨*/
