51474cca3a9a1e2fe6b2e9290ea715e8b07e8ed7
taehyeon0412 <71374539+taehyeon0412@users.noreply.github.com>
2022-07-14 15:03:44 +0900

    Update search.js

--- js/search.js
+++ js/search.js
@@ -12,4 +12,5 @@ function searchSubmit(){
 searchForm.addEventListener("submit",searchSubmit);
 
 
-/*submit 후에 뒤로가기 눌렀을때 input값이 초기화되게 만들어야됨*/
+/*submit 후에 뒤로가기 눌렀을때 input값이 초기화되게 만들어야됨
+  어떻게 하는지 모르겠음.. 추후 수정하기*/
